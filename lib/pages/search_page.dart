74713eda3e64cecb07aa24e2a02370d487b2718c
Domiciano Rincón <domicianorincon@Domicianos-MacBook-Pro.local>
2026-02-23 16:32:46 -0500

    4:32

--- lib/pages/search_page.dart
+++ lib/pages/search_page.dart
@@ -1,4 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:lab2261/components/track_list_item.dart';
+import 'package:lab2261/model/track.dart';
+
+import 'package:http/http.dart' as http;
+import 'dart:convert';
 
 class SearchPage extends StatefulWidget {
   const SearchPage({super.key});
@@ -8,11 +13,41 @@ class SearchPage extends StatefulWidget {
 }
 
 class _SearchPageState extends State<SearchPage> {
+  List<Track> tracks = [];
+  @override
+  void initState() {
+    super.initState();
+    obtenerDato();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Colors.black,
-      body: Center(child: Text('Search Page')),
-    );
+    return Column(children: [Text("Tracks")]);
+  }
+
+  Future<void> obtenerDato() async {
+    final url = Uri.parse("https://api.deezer.com/search?q=eminem");
+
+    final response = await http.get(url);
+
+    if (response.statusCode == 200) {
+      final data = jsonDecode(response.body);
+      List<dynamic> tracksData = data["data"]; //Map<String, dynamic>
+
+      //List<dynamic> -> List<Track>
+      List<Track> tracks = tracksData
+          .map(
+            (e) => Track(
+              id: e["id"],
+              title: e["title"],
+              artist: e["artist"]["name"],
+              album: e["album"]["title"],
+              coverUrl: e["album"]["cover_small"],
+            ),
+          )
+          .toList();
+    } else {
+      print("Error en la petición");
+    }
   }
 }
